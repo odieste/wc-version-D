5fa56f9022cc26beab2d96a0b1077e1b5a5ec0bc
odieste <odieste@fi.upm.es>
2024-10-04 10:21:35 +0200

    Add the count logic to counter

--- src/main/java/es/upm/grise/profundizacion/wc/Counter.java
+++ src/main/java/es/upm/grise/profundizacion/wc/Counter.java
@@ -1,5 +1,36 @@
 package es.upm.grise.profundizacion.wc;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+
 public class Counter {
+	
+	int numberCharacters = 0;
+	int numberLines = 0;
+	int numberWords = 0;
+	
+	public Counter(BufferedReader br) throws IOException {
+
+	    int character = br.read();
+
+	    while (character != -1) {
+	    	// One more character
+	    	numberCharacters++;
+	    	
+	    	// Check if a separator has been found, which means a word
+	    	// has been detected
+	    	if(character == ' ' || character == '\t' || character == '\n') {
+	    		numberWords++;
+	    	}
+	    	
+	    	// Check newline to count lines
+	    	if(character == '\n') {
+	    		numberLines++;
+	    	}
+	    	
+	    	character = br.read();
+	    }
+		
+	}
 	
 }
